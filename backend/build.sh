68aa0832f0f6c368b27c7a97f14355ffd70d01d7
Alazar Zemene <alazarzem2022@gmail.com>
2026-02-08 18:14:13 +0300

    Fix build.sh for Render: remove runserver and set executable bit

--- backend/build.sh
+++ backend/build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
+
+# If we are running from the root (as Render does), go to the backend directory
+if [ -d "backend" ]; then
+  cd backend
+fi
 
 pip install -r requirements.txt
 
 python manage.py collectstatic --noinput
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
