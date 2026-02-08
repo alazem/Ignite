bc556dc5aa553bece53ab77c874121205c077547
Alazar Zemene <alazarzem2022@gmail.com>
2026-02-08 15:17:59 +0300

    Add build script for Render

--- backend/portfolio_backend/settings.py
+++ backend/portfolio_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lyb#!_)o)(37t82dir9!o8lx@)(l18iaxi^@k-j*)(_62=pfdo'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-lyb#!_)o)(37t82dir9!o8lx@)(l18iaxi^@k-j*)(_62=pfdo')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 
 APPEND_SLASH = False
 
@@ -81,10 +82,10 @@ WSGI_APPLICATION = 'portfolio_backend.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 
@@ -145,4 +146,3 @@ SIMPLE_JWT = {
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
 }
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
-
